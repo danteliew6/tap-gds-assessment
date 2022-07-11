f0c0c264e34b41744e92eec423a464858e3e293c
danteliew6 <danteliew6@gmail.com>
2022-07-11 12:14:20 +0900

    Added Bootstrap and clean up repository

--- config.py
+++ config.py
@@ -9,7 +9,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 # Connect to the database
 SQLALCHEMY_DATABASE_URI = environ.get('dbURL') or 'mysql+mysqlconnector://root@localhost:3306/football_competition'
-# SQLALCHEMY_DATABASE_URI = environ.get('dbURL') or 'mysql+mysqlconnector://admin:XYBfSJxQJC5M9zwdbsZq@database-2.cmsaamavflox.us-east-2.rds.amazonaws.com:3306/coursecycle'
 # Turn off the Flask-SQLAlchemy event system and warning
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ENGINE_OPTIONS = {'pool_recycle': 299}
